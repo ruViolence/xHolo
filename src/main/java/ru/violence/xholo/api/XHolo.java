138283d8931ce987baed67e3b68f4c6996fb662b
ruViolence <78062896+ruViolence@users.noreply.github.com>
2024-03-06 05:35:48 +0800

    Add HologramRegistry accessor to XHolo class

--- src/main/java/ru/violence/xholo/api/XHolo.java
+++ src/main/java/ru/violence/xholo/api/XHolo.java
@@ -10,9 +10,15 @@ import ru.violence.xholo.api.impl.VirtualBlockDisplayBuilderImpl;
 import ru.violence.xholo.api.impl.VirtualInteractionBuilderImpl;
 import ru.violence.xholo.api.impl.VirtualItemDisplayBuilderImpl;
 import ru.violence.xholo.api.impl.VirtualTextDisplayBuilderImpl;
+import ru.violence.xholo.api.registry.HologramRegistry;
 
 @UtilityClass
 public class XHolo {
+    @Contract(pure = true)
+    public static @NotNull HologramRegistry getRegistry() {
+        return XHoloPlugin.getInstance().getRegistry();
+    }
+
     @Contract(value = "_ -> new", pure = true)
     public static @NotNull VirtualArmorStandBuilder armorStand(@NotNull Plugin plugin) {
         return new VirtualArmorStandBuilderImpl(plugin, XHoloPlugin.getInstance().getRegistry());
